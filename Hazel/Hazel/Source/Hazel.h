9db3dc7b692cefca25d22f564df7eecc1c1b5ada
Ali El Saleh <elsaleh78@gmail.com>
2018-11-07 12:30:56 +1100

    Create Entry Point to the program
    Remove Test class
    Add Core.h, EntryPoint.h, Application class

---
+++ Hazel/Hazel/Source/Hazel.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// For use by Hazel applications
+
+#include "Hazel-Core/Application.h"
+
+// ====== Entry Point ======
+#include "Hazel-Core/EntryPoint.h"
+// =========================
